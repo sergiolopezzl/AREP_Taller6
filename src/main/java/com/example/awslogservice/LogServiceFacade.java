8b7e5d93436ff380fd954323fc533001737a17ad
unknown <sergio.lopez-v@labinfo.is.escuelaing.edu.co>
2024-03-07 15:55:15 -0500

    v1

---
+++ src/main/java/com/example/awslogservice/LogServiceFacade.java
@@ -0,0 +1,17 @@
+package com.example.awslogservice;
+
+import static spark.Spark.staticFiles;
+import static spark.Spark.get;
+public class LogServiceFacade {
+
+    private static final String LOG_SERVICE_URL = "http://localhost:5000/logservice";
+
+    public static void main(String[] args) {
+        ServiceInvoker invoker = new ServiceInvoker(LOG_SERVICE_URL);
+        staticFiles.location("/public");
+        get("/logservicefacade", (req, res) -> {
+            res.type("application/json");
+            return invoker.invoke(args);
+        });
+    }
+}
---
+++ src/main/java/com/example/awsprimerlogservice/LogService.java
@@ -0,0 +1,38 @@
+package com.example.awsprimerlogservice;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static spark.Spark.*;
+
+public class LogService {
+
+    public static void main(String... args) {
+        MongoDatabase database = ConexionMongoDB.conectar();
+        MongoCollection<Document> collection = database.getCollection("registro");
+
+        port(35000);
+        // get("/guardar", (req,res) -> webClient());
+        get("/logservice", (req, res) -> {
+            res.type("application/json");
+            return "{\"logid1\":\"20-2-2024-Log Inicial\"}";
+        });
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
+    public static String getLogs() {
+
+        return null;
+    }
+
+}
